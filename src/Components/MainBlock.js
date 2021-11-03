ddb01f4c8a32fb1e819b2c48d2809f955b9b9146
kevops <kevin.morales@jala-foundation.org>
2021-11-02 22:24:19 -0500

    I advance until new project

--- src/Components/MainBlock.js
+++ src/Components/MainBlock.js
@@ -1,5 +1,7 @@
 import React from "react";
 import filter from "../assets/img/filter.png";
+import enter from "../assets/img/enter.png";
+import plus from "../assets/img/plus.png";
 
 export default function MainBlock() {
   return (
@@ -8,16 +10,21 @@ export default function MainBlock() {
         <h2>Buenas tardes, Edwin Encinas</h2>
         <p>No estes ocupado, se productivo.</p>
       </div>
-      <div style={{display:"flex", flexDirection:"row"}}>
+      <div style={{display:"flex", flexDirection:"row", alignItems:'center'}}>
         <img src={filter} width="25px" height="25px" alt="home icon" />
-        <h3>Filtrar por nombre</h3>
+        <h3 style={{marginLeft:"5px", fontWeight:"bolder", fontSize:"15px", color:"#766F6F"}}>Filtrar por nombre...</h3>
+      </div>
+      <div style={{display:"flex", flexDirection:"row", alignItems:'center'}}>
+        <h3 style={{marginRight:"5px", fontWeight:"bolder", fontSize:"15px", color:"black"}}>Favoritos</h3>
+        <img src={enter} width="25px" height="25px" alt="home icon" />
       </div>
       <div>
         <h2>Todos</h2>
-        <div>
-          <div>
-            <h3>+</h3>
-            <h3>Agregar nuevo proyecto</h3>
+        <div style={{margin:"10px"}}>
+          <div style={{border:"dotted black 2px", width:'25%', height:"180px", flexDirection:"column"}} className="centered">
+          <img src={plus} width="25px" height="25px" alt="home icon" />
+        <h3 style={{marginLeft:"5px", fontWeight:"bolder", fontSize:"15px", color:"#766F6F"}}>Agregar nuevo proyecto</h3>
+            
           </div>
           <div></div>
           <div></div>
